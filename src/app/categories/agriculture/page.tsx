ab98793b299014b761034b21268b132bcbc5ca88
saslandeco-cmyk <saslandeco@gmail.com>
2026-08-25 14:55:16 +0200

    Delete src/app/categories/agriculture directory

--- src/app/categories/agriculture/page.tsx
+++
@@ -1,6 +0,0 @@
-import CategoryPage from "@/components/category/CategoryPage";
-import { CATEGORY_META } from "@/lib/categoryData";
-
-export default function Page() {
-  return <CategoryPage meta={CATEGORY_META["agriculture"]} />;
-}
